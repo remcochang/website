1ee26de4e218076597a80df836be233628d73b71
Caroline Ziemkiewicz <cziemkiewicz@forrester.com>
2022-01-23 13:59:47 -0500

    Basic Eleventy setup

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = function (config) {
+  config.addPassthroughCopy('img');
+  config.addPassthroughCopy('styles');
+  config.addPassthroughCopy('js');
+  config.addPassthroughCopy('data');
+  return {
+    dir: {
+      input: './',
+      output: 'dist',
+    },
+  };
+};
